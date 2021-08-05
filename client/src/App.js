4ea34a8fd021dc5540475453e90d8e1db4c7e03d
Amit Kumar Upadhyay <66126457+mit1275@users.noreply.github.com>
2021-08-05 19:48:30 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+
+import "./App.css";
+import StudentSigin  from "./components/Screens/Signin/StudentSignin";
+import AdminSignin from "./components/Screens/Signin/AdminSignin";
+import StudentSignup from "./components/Screens/Signup/StudentSignup";
+import AdminSignup from "./components/Screens/Signup/AdminSignup";
+import React  from "react";
+import {BrowserRouter,Link,Route,Switch } from "react-router-dom";
+
 import Footer from "../src/components/footer";
 import Header from "../src/components/header";
 
@@ -17,6 +26,11 @@ function App() {
         <Route path="/home" exact component={home} />
         <Route path="/about" exact component={about} />
         <Route path="/contact" exact component={contact} />
+          
+            <Route path="/studentsignin" component={StudentSigin} />
+            <Route path="/adminsignin" component={AdminSignin} />
+            <Route path="/studentsignup" component={StudentSignup}/>
+            <Route path="/adminsignup" component={AdminSignup}/>
       </Router>
       <Footer />
     </div>
