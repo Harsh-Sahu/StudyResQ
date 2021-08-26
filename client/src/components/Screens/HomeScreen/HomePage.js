4707da33073a0f57a8b5dec4a6b0deae2c2dcab3
Harsh Sahu <harshsahuhs1999@gmail.com>
2021-08-26 12:29:37 +0530

    Modified frontend by prateek added library display

--- client/src/components/Screens/HomeScreen/HomePage.js
+++ client/src/components/Screens/HomeScreen/HomePage.js
@@ -1,11 +1,11 @@
-import React, { useCallback } from "react";
-import Axios from "axios";
+import React, { useCallback } from 'react';
+import Axios from 'axios';
 import Card from '@material-ui/core/Card';
-import Link from "@material-ui/core/Link";
-import { useHistory } from "react-router-dom";
-import { makeStyles } from "@material-ui/core/styles";
-import { toast } from "react-toastify";
-import { Toast } from "bootstrap";
+import Link from '@material-ui/core/Link';
+import { useHistory } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import { toast } from 'react-toastify';
+import { Toast } from 'bootstrap';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
@@ -14,7 +14,7 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 toast.configure();
 
-const useStyles = makeStyles(theme=>({
+const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     padding: theme.spacing(2),
@@ -25,54 +25,48 @@ const useStyles = makeStyles(theme=>({
   },
 }));
 
-
 function MediaCard(props) {
   const classes = useStyles();
   // const [id, setid] = React.useState(props._id);
-  const [name,setname]=React.useState("kl");
-  
- 
-
+  const [name, setname] = React.useState('kl');
 
   return (
     <>
       {
-        <Link href={"http://localhost:3001/files/"+props.filename}>
-       <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="/public/images/book.jpg"
-          title={props.filename}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-          <h4>Book Name</h4>
-          {props.filename}
-           <p></p>
-          <h4>Book ID</h4>
-          {props._id}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-          
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-      </CardActions>
-    </Card>
-    </Link>
+        <Link href={'http://localhost:3001/files/' + props.filename}>
+          <Card className={classes.root}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image='/public/images/book.jpg'
+                title={props.filename}
+              />
+              <CardContent>
+                <Typography gutterBottom variant='h5' component='h2'>
+                  <h4>Book Name</h4>
+                  {props.filename}
+                  <p></p>
+                  <h4>Book ID</h4>
+                  {props._id}
+                </Typography>
+                <Typography
+                  variant='body2'
+                  color='textSecondary'
+                  component='p'
+                ></Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions></CardActions>
+          </Card>
+        </Link>
       }
     </>
   );
 }
 
-
-
-
 export default function HomePage() {
   const [Files, setFiles] = React.useState([]);
-  const address = "http://localhost:3001/allfiles";
+  const address = 'http://localhost:3001/allfiles';
   React.useEffect(() => {
     Axios.post(address).then((result) => {
       console.log(result);
@@ -82,17 +76,17 @@ export default function HomePage() {
 
   return (
     <div>
-      {(
-          <div style={{ margin: "auto", width: "90%" }}>
-            {Files.slice(0, 20).map((item) => {
-              return (
-                <>
-                  <MediaCard {...item} />
-                </>
-              );
-            })}
-          </div>
-      )}
+      {
+        <div style={{ margin: 'auto', width: '90%' }}>
+          {Files.slice(0, 20).map((item) => {
+            return (
+              <>
+                <MediaCard {...item} />
+              </>
+            );
+          })}
+        </div>
+      }
     </div>
   );
 }
